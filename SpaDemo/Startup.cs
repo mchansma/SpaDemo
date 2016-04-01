8880b1ae3decfb7b79a5549e6207f486f352020c
MC Hansma <marco.hansma@gmail.com>
2016-04-01 12:29:38 +0200

    Project Created

---
+++ SpaDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpaDemo.Startup))]
+namespace SpaDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
